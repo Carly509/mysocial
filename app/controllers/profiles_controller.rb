e81500052ae7ed1866010337ea1235b4578d9276
carly509 <tesnorcarly@gmail.com>
2024-11-15 16:12:35 -0500

    added authentification page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,70 +1,36 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_profile, only: [:show, :edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
-  # GET /profiles or /profiles.json
-  def index
-    @profiles = Profile.all
-  end
-
-  # GET /profiles/1 or /profiles/1.json
   def show
+    @posts = @profile.user.posts.order(created_at: :desc).includes(:user)
   end
 
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
-  # GET /profiles/1/edit
   def edit
   end
 
-  # POST /profiles or /profiles.json
-  def create
-    @profile = Profile.new(profile_params)
-
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to @profile, notice: "Profile was successfully created." }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /profiles/1 or /profiles/1.json
   def update
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: "Profile was successfully updated." }
-        format.json { render :show, status: :ok, location: @profile }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /profiles/1 or /profiles/1.json
-  def destroy
-    @profile.destroy
-
-    respond_to do |format|
-      format.html { redirect_to profiles_path, status: :see_other, notice: "Profile was successfully destroyed." }
-      format.json { head :no_content }
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: 'Profile was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile
-      @profile = Profile.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def profile_params
-      params.require(:profile).permit(:user_id, :bio)
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def ensure_correct_user
+    unless @profile.user == current_user
+      redirect_to root_path, alert: 'You are not authorized to perform this action.'
     end
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :bio, :avatar)
+  end
 end
